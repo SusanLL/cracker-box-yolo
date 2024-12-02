6b016da4e4bdba5b949c7803881787e3514fc4d6
SusanLL <146895775+SusanLL@users.noreply.github.com>
2024-12-02 17:34:09 -0600

    Update loss.py

--- yolo/loss.py
+++ yolo/loss.py
@@ -6,9 +6,6 @@ import os
 import torch
 import torch.nn as nn
 
-
-# compute Intersection over Union (IoU) of two bounding boxes
-# the input bounding boxes are in (cx, cy, w, h) format
 def compute_iou(pred, gt):
     x1p = pred[0] - pred[2] * 0.5
     x2p = pred[0] + pred[2] * 0.5
@@ -32,74 +29,92 @@ def compute_iou(pred, gt):
     inter = w * h
     iou = inter / (areap + areag - inter)    
     return iou
-
-# TODO: finish the implementation of this loss function for YOLO training
-# output: (batch_size, num_boxes * 5 + num_classes, 7, 7), raw output from the network
-# pred_box: (batch_size, num_boxes * 5 + num_classes, 7, 7), predicted bounding boxes from the network (see the forward() function)
-# gt_box: (batch_size, 5, 7, 7), ground truth bounding box target from the dataloader
-# gt_mask: (batch_size, 7, 7), ground truth bounding box mask from the dataloader
-# num_boxes: number of bounding boxes per cell
-# num_classes: number of object classes for detection
-# grid_size: YOLO grid size, 64 in our case
-# image_size: YOLO image size, 448 in our case
+    
 def compute_loss(output, pred_box, gt_box, gt_mask, num_boxes, num_classes, grid_size, image_size):
     batch_size = output.shape[0]
     num_grids = output.shape[2]
-    # compute mask with shape (batch_size, num_boxes, 7, 7) for box assignment
+
+    # Masks for bounding box assignments
     box_mask = torch.zeros(batch_size, num_boxes, num_grids, num_grids)
     box_confidence = torch.zeros(batch_size, num_boxes, num_grids, num_grids)
 
-    # compute assignment of predicted bounding boxes for ground truth bounding boxes
+    # Assign predicted boxes to ground truth boxes
     for i in range(batch_size):
         for j in range(num_grids):
             for k in range(num_grids):
- 
-                # if the gt mask is 1
-                if gt_mask[i, j, k] > 0:
-                    # transform gt box
+                if gt_mask[i, j, k] > 0:  # If a ground truth box exists
                     gt = gt_box[i, :, j, k].clone()
                     gt[0] = gt[0] * grid_size + k * grid_size
                     gt[1] = gt[1] * grid_size + j * grid_size
                     gt[2] = gt[2] * image_size
                     gt[3] = gt[3] * image_size
-                    # print('gt in loss %.2f, %.2f, %.2f, %.2f' % (gt[0], gt[1], gt[2], gt[3]))
 
                     select = 0
                     max_iou = -1
-                    # select the one with maximum IoU
+
                     for b in range(num_boxes):
-                        # center x, y and width, height
-                        pred = pred_box[i, 5*b:5*b+4, j, k].clone()
+                        pred = pred_box[i, 5 * b:5 * b + 4, j, k].clone()
                         iou = compute_iou(gt, pred)
                         if iou > max_iou:
                             max_iou = iou
                             select = b
+
                     box_mask[i, select, j, k] = 1
                     box_confidence[i, select, j, k] = max_iou
-                    print('select box %d with iou %.2f' % (select, max_iou))
 
-    # compute yolo loss
+    # Weights for the loss components
     weight_coord = 5.0
     weight_noobj = 0.5
 
-    # according to the YOLO paper, we compute the following losses
-    # loss_x: loss function on x coordinate (cx)
-    # loss_y: loss function on y coordinate (cy)
-    # loss_w: loss function on width 
-    # loss_h: loss function on height
-    # loss_obj: loss function on confidence for objects
-    # loss_nonobj: loss function on confidence for non-objects
-    # loss_cls: loss function for object class
+    # Loss components
+    loss_x = torch.sum(
+        weight_coord
+        * box_mask
+        * torch.pow(output[:, 0:5 * num_boxes:5] - gt_box[:, 0:1, :, :], 2.0)
+    )
+    loss_y = torch.sum(
+        weight_coord
+        * box_mask
+        * torch.pow(output[:, 1:5 * num_boxes:5] - gt_box[:, 1:2, :, :], 2.0)
+    )
+    loss_w = torch.sum(
+        weight_coord
+        * box_mask
+        * torch.pow(
+            torch.sqrt(output[:, 2:5 * num_boxes:5]) - torch.sqrt(gt_box[:, 2:3, :, :]),
+            2.0,
+        )
+    )
+    loss_h = torch.sum(
+        weight_coord
+        * box_mask
+        * torch.pow(
+            torch.sqrt(output[:, 3:5 * num_boxes:5]) - torch.sqrt(gt_box[:, 3:4, :, :]),
+            2.0,
+        )
+    )
+    loss_obj = torch.sum(
+        box_mask * torch.pow(box_confidence - output[:, 4:5 * num_boxes:5], 2.0)
+    )
+    loss_noobj = torch.sum(
+        weight_noobj
+        * (1 - box_mask)
+        * torch.pow(output[:, 4:5 * num_boxes:5], 2.0)
+    )
 
-    # This is implementation for the loss_obj
-    # Follow this example to compute other losses
-    loss_obj = torch.sum(box_mask * torch.pow(box_confidence - output[:, 4:5*num_boxes:5], 2.0))
+    # Classification loss
+    loss_cls = torch.sum(
+        gt_mask.unsqueeze(1)
+        * torch.pow(output[:, 5 * num_boxes:] - gt_box[:, 4:, :, :], 2.0)
+    )
 
-    ### ADD YOUR CODE HERE ###
-    # Use weight_coord and weight_noobj defined above
-
-    # print('lx: %.4f, ly: %.4f, lw: %.4f, lh: %.4f, lobj: %.4f, lnoobj: %.4f, lcls: %.4f' % (loss_x, loss_y, loss_w, loss_h, loss_obj, loss_noobj, loss_cls))
-
-    # the totol loss
+    # Total loss
     loss = loss_x + loss_y + loss_w + loss_h + loss_obj + loss_noobj + loss_cls
+
+    # Print for debugging
+    print(
+        "lx: %.4f, ly: %.4f, lw: %.4f, lh: %.4f, lobj: %.4f, lnoobj: %.4f, lcls: %.4f"
+        % (loss_x.item(), loss_y.item(), loss_w.item(), loss_h.item(), loss_obj.item(), loss_noobj.item(), loss_cls.item())
+    )
+
     return loss
