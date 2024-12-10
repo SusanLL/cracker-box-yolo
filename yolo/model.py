3441c1236673cf7fe5dc8072670e6ee322fb5fc2
SusanLL <146895775+SusanLL@users.noreply.github.com>
2024-12-10 13:31:05 -0600

    Update model.py

--- yolo/model.py
+++ yolo/model.py
@@ -8,7 +8,6 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-
 # the YOLO network class
 class YOLO(nn.Module):
     def __init__(self, num_boxes, num_classes):
@@ -30,8 +29,7 @@ class YOLO(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-
-    #TODO: implement this function to build the network
+    # Implement the create_modules function to build the network
     def create_modules(self):
         modules = nn.Sequential()
 
@@ -94,11 +92,7 @@ class YOLO(nn.Module):
 
         return modules
 
-
-
-    # output (batch_size, 5*B + C, 7, 7)
-    # In the network output (cx, cy, w, h) are normalized to be [0, 1]
-    # This function undo the noramlization to obtain the bounding boxes in the orignial image space
+    # Undo normalization to obtain bounding boxes in the original image space
     def transform_predictions(self, output):
         batch_size = output.shape[0]
         x = torch.linspace(0, 384, steps=7)
@@ -107,30 +101,22 @@ class YOLO(nn.Module):
         corner_x = torch.unsqueeze(corner_x, dim=0)
         corner_y = torch.unsqueeze(corner_y, dim=0)
         corners = torch.cat((corner_x, corner_y), dim=0)
-        # corners are top-left corners for each cell in the grid
         corners = corners.unsqueeze(0).repeat(batch_size, 1, 1, 1)
         pred_box = output.clone()
 
-        # for each bounding box
         for i in range(self.num_boxes):
-            # x and y
             pred_box[:, i*5, :, :] = corners[:, 0, :, :] + output[:, i*5, :, :] * self.grid_size
             pred_box[:, i*5+1, :, :] = corners[:, 1, :, :] + output[:, i*5+1, :, :] * self.grid_size
-            # w and h
             pred_box[:, i*5+2, :, :] = output[:, i*5+2, :, :] * self.image_size
             pred_box[:, i*5+3, :, :] = output[:, i*5+3, :, :] * self.image_size
 
         return pred_box
 
-
     # forward pass of the YOLO network
     def forward(self, x):
-        # raw output from the network
         output = self.network(x).reshape((-1, self.num_boxes * 5 + self.num_classes, 7, 7))
-        # compute bounding boxes in the original image space
         pred_box = self.transform_predictions(output)
         return output, pred_box
-
 
 # run this main function for testing
 if __name__ == '__main__':
@@ -139,7 +125,7 @@ if __name__ == '__main__':
 
     image = np.random.uniform(-0.5, 0.5, size=(1, 3, 448, 448)).astype(np.float32)
     image_tensor = torch.from_numpy(image)
-    print('input image:', image_tensor.shape)
+    print('Input image:', image_tensor.shape)
 
     output, pred_box = network(image_tensor)
-    print('network output:', output.shape, pred_box.shape)
+    print('Network output:', output.shape, pred_box.shape)
